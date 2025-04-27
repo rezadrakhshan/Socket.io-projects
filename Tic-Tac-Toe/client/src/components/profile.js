0a9e857c738adcc033b7639e4e8653d66f976f4e
unknown <github2reza@gmail.com>
2025-04-27 05:53:15 -0800

    feat
    Complete Update profile section

--- Tic-Tac-Toe/client/src/components/profile.js
+++ Tic-Tac-Toe/client/src/components/profile.js
@@ -1,9 +1,35 @@
 const form = document.querySelector("#ProfileForm");
+const avatarInput = document.getElementById("avatarInput");
+const preview = document.getElementById("preview");
+
+avatarInput.addEventListener("change", () => {
+  const file = avatarInput.files[0];
+  if (file) {
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      preview.src = e.target.result;
+    };
+    reader.readAsDataURL(file);
+  }
+});
+
+
+export function showToast(message, type = "success") {
+  const toast = document.createElement("div");
+  toast.classList.add("toast", type);
+  toast.textContent = message;
+
+  document.getElementById("toast-container").appendChild(toast);
+
+  setTimeout(() => {
+    toast.remove();
+  }, 4000);
+}
 
 form.addEventListener("submit", async (e) => {
   e.preventDefault();
-  const username = e.target.username.value;
-  const password = e.target.password.value ? e.target.password.value : "";
+  const username = e.target.elements.username.value;
+  const password = e.target.elements.password?.value || "";
   const avatarFile = document.getElementById("avatarInput").files[0];
 
   const formData = new FormData();
@@ -16,5 +42,11 @@ form.addEventListener("submit", async (e) => {
     body: formData,
   });
   const data = await response.json();
-  console.log(data);
+  if (response.ok) {
+    showToast("Profile Updated")
+  }
+  else{
+    showToast(data.message,"error")
+  }
 });
+
--- Tic-Tac-Toe/server/controller/profile.js
+++ Tic-Tac-Toe/server/controller/profile.js
@@ -1,12 +1,33 @@
+import _ from "lodash";
+import User from "../models/user.js";
+import { __dirname } from "../server.js";
+import bcrypt from "bcrypt";
+
 export default new (class {
   async updateAvatar(req, res) {
-    if (!req.file) {
-      return res
-        .status(400)
-        .json({ success: false, message: "No file uploaded" });
-    }
+    try {
+      const data = _.pick(req.body, ["username", "password"]);
+      const user = await User.findById(req.user.id);
 
-    const profilePath = "/public/image/users/" + req.file.filename;
-    res.json({ success: true, profilePath });
+      if (req.file) {
+        data.profile = `/public/image/users/${req.file.filename}`;
+      }
+
+      if (data.password) {
+        const salt = await bcrypt.genSalt(10);
+        data.password = await bcrypt.hash(data.password, salt);
+      }
+
+      const result = await User.findByIdAndUpdate(req.user.id, data, {
+        new: true,
+      });
+
+      return res.json(result);
+    } catch (error) {
+      if (error.code === 11000 && error.keyPattern?.username) {
+        return res.status(400).json({ message: "Username already exists." });
+      }
+      return res.status(500).json({ message: error.message });
+    }
   }
 })();
