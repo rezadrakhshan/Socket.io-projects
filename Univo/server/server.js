a657c98d37002c9fc42e57ee764a78ab085ff86b
unknown <github2reza@gmail.com>
2025-06-08 21:36:24 +0330

    Initial commit

---
+++ Univo/server/server.js
@@ -0,0 +1,23 @@
+import e from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import debug from "debug";
+import path from "path";
+import logging from "./start/logging.js";
+import config from "./start/config.js";
+
+const app = e();
+const port = process.env.PORT || 3000;
+const server = createServer(app);
+const io = new Server(server);
+export const log = debug("app:main");
+export const __dirname = path.resolve();
+
+logging();
+config(app, e);
+
+io.on("connection", (socket) => {
+  log("user connected");
+});
+
+server.listen(port, () => log(`app running on port ${port}`));
---
+++ Univo/server/start/config.js
@@ -0,0 +1,12 @@
+import helmet from "helmet";
+import { __dirname } from "../server.js";
+import path from "path";
+
+export default function (app, e) {
+  app.set("view engine", "ejs");
+  app.set("views", path.join(__dirname, "client/public/templates"));
+  app.use(helmet());
+  app.use(e.urlencoded({ extended: true }));
+  app.use(e.json());
+  app.use(e.static("client"));
+}
