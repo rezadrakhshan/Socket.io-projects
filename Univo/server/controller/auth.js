4e118fbedf99d17ded2425ef4e0e8c9b2ed36f3b
unknown <github2reza@gmail.com>
2025-06-09 16:17:44 +0330

    feat(auth): Devloped endpoint for login user

--- Univo/server/controller/auth.js
+++ Univo/server/controller/auth.js
@@ -1,6 +1,8 @@
 import _ from "lodash";
 import User from "../models/user.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import c from "config";
 
 export default new (class {
   async auth(req, res) {
@@ -17,10 +19,27 @@ export default new (class {
     const saltRound = await bcrypt.genSalt(10);
     data.password = await bcrypt.hash(data.password, saltRound);
     user = await new User(data);
-    await user.save()
+    await user.save();
+    const payload = { id: user.id };
     return res.json({
       msg: "user created",
-      data: _.pick(user, ["email", "fullname"]),
+      token: jwt.sign(payload, c.get("jwt_secret")),
+    });
+  }
+  async login(req, res) {
+    const data = _.pick(req.body, ["email", "password"]);
+    const user = await User.findOne({ email: data.email });
+    if (!user) {
+      return res.status(401).json({ msg: "Invalid email or password" });
+    }
+    const isMatch = await bcrypt.compare(data.password, user.password);
+    if (!isMatch) {
+      return res.status(401).json({ msg: "Invalid email or password" });
+    }
+    const payload = { id: user.id };
+    return res.json({
+      msg: "login successfuly",
+      token: jwt.sign(payload, c.get("jwt_secret")),
     });
   }
 })();
--- Univo/server/routes/auth.js
+++ Univo/server/routes/auth.js
@@ -5,5 +5,6 @@ const router = Router();
 
 router.get("/", controller.auth);
 router.post("/register", controller.register);
+router.post("/login", controller.login);
 
 export default router;
