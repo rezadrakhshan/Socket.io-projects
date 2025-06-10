4c3b15b6912850465ef9a3036d5766092effe3a0
unknown <github2reza@gmail.com>
2025-06-10 10:31:39 +0330

    feat(about): add about page

--- Univo/server/controller/index.js
+++ Univo/server/controller/index.js
@@ -2,4 +2,7 @@ export default new (class {
   async home(req, res) {
     res.render("index");
   }
+  async about(req, res) {
+    res.render("about");
+  }
 })();
--- Univo/server/routes/index.js
+++ Univo/server/routes/index.js
@@ -5,6 +5,7 @@ import authRouter from "./auth.js";
 const router = e.Router();
 
 router.get("/", controller.home);
+router.get("/about", controller.about);
 router.use("/auth", authRouter);
 
 export default router;
